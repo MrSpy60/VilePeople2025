00aa3a6764956ab6d56df50b5d46331c3511afc4
[khalid yusuf] <[khalidy289]>
2025-01-17 10:38:26 +0100

    status interface

---
+++ PeopleVilleEngine/Status/IStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleVilleEngine.Status
+{
+    public interface IStatus
+    {
+        public void effecttrigger(Village village); 
+
+
+    }
+}   
