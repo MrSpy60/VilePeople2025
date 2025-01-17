801c82e60701f7d3f672a5ef2ec3c1d1b83451b8
Jakob <lyngsoejakob@gmail.com>
2025-01-17 11:25:35 +0100

    VillagerStats_Test

---
+++ PeopleVilleEngine/Villagers/VillagerStats/VillagerStats.cs
@@ -0,0 +1,18 @@
+﻿namespace PeopleVilleEngine.Villagers.VillagerStats
+{
+    public class VillagerStats
+    {
+        public VillagerHealth Health { get; private set; }
+        public VillagerWorkEfficiency WorkEfficiency { get; private set; }
+
+        public VillagerStats(RNG rng)
+        {
+            int maxHealth = 100; // Max hp is 100
+            int currentHealth = rng.Next(1, maxHealth + 1); // current HP is random
+            double efficiency = rng.NextDouble(); // Random efficiency
+
+            Health = new VillagerHealth(maxHealth, currentHealth);
+            WorkEfficiency = new VillagerWorkEfficiency(efficiency);
+        }
+    }
+}
---
+++ PeopleVilleEngine/Villagers/VillagerStats/VillagerWorkEfficiency.cs
@@ -0,0 +1,17 @@
+﻿namespace PeopleVilleEngine.Villagers.VillagerStats
+{
+    public class VillagerWorkEfficiency
+    {
+        public double Efficiency { get; private set; }
+
+        public VillagerWorkEfficiency(double efficiency)
+        {
+            Efficiency = efficiency;
+        }
+
+        public void AdjustEfficiency(double adjustment)
+        {
+            Efficiency = Math.Max(0, Efficiency + adjustment);
+        }
+    }
+}
