631aa60d95b7f3330ad34c96c1f79018e25e01db
jesperweble@outlook.com <jesperweble@outlook.com>
2025-01-17 08:22:34 +0100

    Added interface for Workplaces. Added workplace: Food Station

---
+++ PeopleVilleEngine/Locations/FoodStation.cs
@@ -0,0 +1,20 @@
+﻿namespace PeopleVilleEngine.Locations;
+public class FoodStation : IWorkplace
+{
+    public FoodStation()
+    {
+        var random = RNG.GetInstance();
+        MaxWorkers = random.Next(1, 3);
+    }
+    private readonly List<BaseVillager> _villagers = new();
+    public string Name => $"Food Station, with {Workers} workers.";
+    public int WorkCost => 50;
+
+    public List<BaseVillager> Villagers()
+    {
+        return _villagers;
+    }
+
+    public int Workers => _villagers.Count;
+    public int MaxWorkers { get; set; }
+}
--- PeopleVilleEngine/Locations/ILocation.cs
+++ PeopleVilleEngine/Locations/ILocation.cs
@@ -2,11 +2,21 @@
 public interface ILocation
 {
     string Name { get; }
+
+    int WorkCost { get; }
+
     List<BaseVillager> Villagers();
 }
 
 public interface IHouse : ILocation
 {
     public int Population { get; }
-    public int MaxPopulation { get; set; }
+    public int MaxPopulation { get; }
+}
+
+public interface IWorkplace : ILocation
+{
+    public int Workers { get; }
+    public int MaxWorkers { get; }
+
 }
