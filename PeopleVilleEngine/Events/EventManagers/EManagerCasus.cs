c8037a979a4110742540822d98b64226ed548019
MrSpy60 <mrspy60@gmail.com>
2025-01-17 08:26:30 +0100

    IEvent

---
+++ PeopleVilleEngine/Events/EventManagers/EManagerCasus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleVilleEngine.Events.EventManagers
+{
+    internal class EManagerCasus : IEventManager
+    {
+        public string Name => "Casus";
+
+        public void TriggerEventManager(Village village)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PeopleVilleEngine/Events/eventDeath.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PeopleVilleEngine.Locations;
+
+namespace PeopleVilleEngine.Events
+{
+    public class eventDeath : IEvent
+    {
+        public EventType Type => EventType.Random;
+
+        public void triggerEvent(Village village)
+        {
+            RNG rng = RNG.GetInstance();
+            int whoDead = rng.Next(0, village.Villagers.Count() - 1);
+            BaseVillager deadManWalking = village.Villagers[whoDead];
+            Console.WriteLine($"{deadManWalking.ToString()} died");
+            ILocation? home = deadManWalking.Home;
+            if(home != null)
+            {
+                home.Villagers().Remove( deadManWalking );
+            }
+            village.Villagers.Remove(deadManWalking);
+        }
+
+        
+    }
+}
