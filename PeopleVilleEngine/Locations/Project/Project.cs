110d1e11a0e38f5a01cb1a415600194f9fcdbb26
Gabriel Ganderup <gabrielsogaard@gmail.com>
2025-01-20 13:00:06 +0100

    Not ready

---
+++ PeopleVilleEngine/Locations/Project/Project.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PeopleVilleEngine.Locations;
+using static PeopleVilleEngine.Locations.Project.BaseProject;
+
+namespace PeopleVilleEngine.Locations.Project
+{
+    public class Project
+    {
+        public ILocation ProjectType { get; }
+        public double CurrentProgress { get; private set; }
+
+        private List<IProjects> Projects { get; } = new();
+
+        public Project(ILocation projectType)
+        {
+            ProjectType = projectType;
+            CurrentProgress = 0;
+        }
+
+        public void Work(double work)
+        {
+            CurrentProgress += work;
+            if (CurrentProgress >= ProjectType.BuildCost)
+            {
+                CurrentProgress = ProjectType.BuildCost; // ensures it doesn't exceed BuildCost
+                RemoveCompletedProject();
+            }
+        }
+
+        public void RemoveCompletedProject()
+        {
+            var completedProject = Projects.FirstOrDefault(p => p.IsComplete);
+            if (completedProject != null)
+            {
+                Projects.Remove(completedProject);
+            }
+        }
+    }
+
+
+    public class SampleProject : IProjects
+    {
+        public double BuildCost { get; set; }
+        public double CurrentProgress { get; set; }
+
+        public bool IsComplete => CurrentProgress >= BuildCost;
+
+        public SampleProject(double buildCost)
+        {
+            BuildCost = buildCost;
+            CurrentProgress = 0;
+        }
+
+        public void AddProgress(double progress)
+        {
+            CurrentProgress += progress;
+        }
+    }
+}
--- PeopleVilleEngine/Village.cs
+++ PeopleVilleEngine/Village.cs
@@ -4,6 +4,7 @@ using PeopleVilleEngine.Locations;
 using System.Reflection;
 using System.Linq;
 using PeopleVilleEngine.Events;
+using PeopleVilleEngine.Locations.Project;
 
 public class Village
 {
@@ -11,20 +12,13 @@ public class Village
     public List<BaseVillager> Villagers { get; } = new();
     public List<ILocation> Locations { get; } = new();
     public VillagerNames VillagerNameLibrary { get; } = VillagerNames.GetInstance();
+
     public Village()
     {
         Console.WriteLine("Creating villager");
         CreateVillage();
+        TestBuildingHealingStation(); // Call the test method here
     }
-
-
-    // get villagers efficiency 
-    // create a formula that calculates the building cost/speed    ->    // template: BuildCost = VillagerAdultCount * VillagerWorkEfficiency
-    public class Project
-    {
-
-    }
-
 
     private void CreateVillage()
     {
@@ -53,14 +47,14 @@ public class Village
     private List<IVillagerCreator> LoadVillagerCreatorFactories()
     {
         var villageCreators = new List<IVillagerCreator>();
-        //Load from this Assembly
+        // Load from this Assembly
         IEnumerable<Type> types = AppDomain.CurrentDomain.GetAssemblies()
             .SelectMany(s => s.GetTypes());
         LoadVillagerCreatorFactoriesFromType(
             AppDomain.CurrentDomain.GetAssemblies().SelectMany(s => s.GetTypes()),
             villageCreators);
 
-        //Load from library Files
+        // Load from library Files
         var libraryFiles = Directory.EnumerateFiles("lib").Where(f => Path.GetExtension(f) == ".dll");
         foreach (var libraryFile in libraryFiles)
         {
@@ -77,13 +71,33 @@ public class Village
         var createrTypes = inputTypes.Where(p => createVillagerInterface.IsAssignableFrom(p) && !p.IsInterface).ToList();
         foreach (var type in createrTypes)
         {
-            Console.WriteLine($"Village Creeater loaded: {type}");
+            Console.WriteLine($"Village Creator loaded: {type}");
             outputVillagerCreators.Add((IVillagerCreator)Activator.CreateInstance(type));
         }
     }
 
+    private void TestBuildingHealingStation()
+    {
+        // Create a HealingStation instance
+        HealingStation healingStation = new HealingStation();
+        Console.WriteLine($"Created: {healingStation.Name}");
+
+        // Create a Project for the HealingStation
+        Project project = new Project(healingStation);
+
+        // Simulate working on the project
+        double workAmount = 21; // Amount of work to simulate
+        while (project.CurrentProgress < healingStation.BuildCost)
+        {
+            project.Work(workAmount);
+            Console.WriteLine($"Current Progress: {project.CurrentProgress}/{healingStation.BuildCost}");
+        }
+
+        Console.WriteLine("Project completed!");
+    }
+
     public override string ToString()
     {
-        return $"Village have {Villagers.Count} villagers, where {Villagers.Count(v => v.HasHome() == false)} are homeless.";
+        return $"Village has {Villagers.Count} villagers, where {Villagers.Count(v => v.HasHome() == false)} are homeless.";
     }
 }
